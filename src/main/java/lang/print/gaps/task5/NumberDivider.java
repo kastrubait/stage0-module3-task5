920984f552997696b1434c2672ca2acb480841e1
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-08 11:53:07 +0300

    feat: complete tast5 module3 v.0

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        final int FIVE = 5;
+        double result = toBeDivided / FIVE;
+        System.out.println(result);
     }
 }
